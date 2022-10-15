be218f508697c4895ac9dd073bbbf2dfcaf46a01
keisukehasegawa0918 <chiro9689@gmail.com>
2022-10-15 09:48:08 +0900

    top画面表示機能作成

---
+++ src/main/java/com/dmm/task/controller/TaskController.java
@@ -0,0 +1,83 @@
+package com.dmm.task.controller;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dmm.task.data.entity.Tasks;
+import com.dmm.task.data.repository.TaskRepository;
+
+@Controller
+public class TaskController {
+	
+	@Autowired
+	private TaskRepository taskRepository;
+
+	@GetMapping("/main")
+	public String main(Model model) {
+		List<List<LocalDate>> matrix = new ArrayList<List<LocalDate>>();
+		List<LocalDate> week = new ArrayList<LocalDate>();
+		
+		// 当月の1日を取得
+		LocalDate day = LocalDate.now().withDayOfMonth(1);
+		model.addAttribute("month", day.getYear() + "年" + day.getMonthValue() + "月");
+		// 曜日を取得
+		DayOfWeek w = day.getDayOfWeek();
+		// 前月分を取得
+		day = day.minusDays(w.getValue());
+		
+		// 1週間分
+		for (int i = 1; i <= 7; i++) {
+			week.add(day);
+			day = day.plusDays(1);
+		}
+		// 全体に追加
+		matrix.add(week);
+		// 次週分を作成
+		week = new ArrayList<LocalDate>();
+		
+		// 当月の日数を取得
+		int lastDay = day.lengthOfMonth();
+		
+		// 2週目以降
+		for (int i = 1; i <= lastDay; i++) {
+			week.add(day);
+			w = day.getDayOfWeek();
+			if (w == DayOfWeek.SATURDAY) {
+				matrix.add(week);
+				week = new ArrayList<LocalDate>();
+			}
+			day = day.plusDays(1);
+		}
+		
+		// 翌月分を取得
+		w = day.getDayOfWeek();
+		System.out.println(w);
+		int nextMonthDays = 7 - w.getValue();
+		System.out.println(nextMonthDays);
+		
+		for (int i = 1; i <= nextMonthDays; i++) {
+			week.add(day);
+			w = day.getDayOfWeek();
+			if (w == DayOfWeek.SATURDAY) {
+				matrix.add(week);
+				break;
+			}
+			day = day.plusDays(1);
+		}
+		
+		model.addAttribute("matrix", matrix);
+		
+		MultiValueMap<LocalDate, Tasks> tasks = new LinkedMultiValueMap<LocalDate, Tasks>();
+		model.addAttribute("tasks", tasks);
+		return "main";
+	}
+}
